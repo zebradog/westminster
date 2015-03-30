d0c03c2d29f069c6f01bab74aaf2855b47a81ff5
Kyle Bertelsen <kyle.bertelsen@gmail.com>
2015-03-30 09:48:25 -0400

    fixing path to layout.css

--- themes/custom/zd_ui/templates/html.tpl.php
+++ themes/custom/zd_ui/templates/html.tpl.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php print $head; ?>
-  <link href="<?php echo path_to_theme(); ?>/css/layout.css" rel="stylesheet" type="text/css">
+  <link href="/<?php echo path_to_theme(); ?>/css/layout.css" rel="stylesheet" type="text/css">
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <!-- HTML5 element support for IE6-8 -->
